61c5b1d05de2def2043e2e68d6c48489b741e471
hgx <david.schiminski@hexfourty.com>
2021-10-29 11:50:38 +0200

    setup scripts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,5 @@
 import { JustFormsCore } from '../index';
 
 test('Index greeter', () => {
-  expect(JustFormsCore('HexFourty')).toEqual('Jo moin HexFourty')
-})
+  expect(JustFormsCore('HexFourty')).toEqual('Jo moin HexFourty');
+});
