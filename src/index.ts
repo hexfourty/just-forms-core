66060dae460b4b4e0af1396b47c5cbb933e058e8
hgx <david.schiminski@hexfourty.com>
2021-11-08 22:14:57 +0100

    added new domain models

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { JustFormsCore } from './JustFormsCore';
+import { JfQuestionnaire } from './domain/questionnaire.model';
+import { JfQuestion } from './domain/question.model';
+import { JfAnswerOption } from './domain/answer-option.model';
+import { JfSection } from './domain/section.model';
 
 export default JustFormsCore;
-Object.assign(module.exports, JustFormsCore)
+Object.assign(module.exports, {JustFormsCore, JfQuestionnaire, JfSection, JfQuestion, JfAnswerOptions})
--- test-build.js
+++ test-build.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
 const JustFormsCore = require('./lib');
+const JfQuestionnaire = require('./lib/domain/questionnaire.model')
 
 const result = JustFormsCore.greet('HexFourty');
 assert.equal(result, 'Jo moin HexFourty')
