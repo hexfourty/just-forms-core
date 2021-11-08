4d911f210bda6cd58f446e25ba8485678a5c0a5b
hgx <david.schiminski@hexfourty.com>
2021-11-08 22:22:00 +0100

    refactored class

--- src/JustFormsCore.ts
+++ src/JustFormsCore.ts
@@ -1,5 +1,9 @@
-export namespace JustFormsCore {
-  export function greet(name: string): string {
+export interface JustFormsCore {
+  greet(name:string):string;
+}
+
+export class JustFormsCore {
+  static greet(name: string) {
     return `Jo moin ${name}`;
   }
 }
