c060614e2b692d982ec8f391eea6f297c7826200
hgx <david.schiminski@hexfourty.com>
2021-11-08 22:28:42 +0100

    fixing exports

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,8 @@ import { JfAnswerOption } from './domain/answer-option.model';
 import { JfSection } from './domain/section.model';
 
 export default JustFormsCore;
-Object.assign(module.exports, { JustFormsCore, JfQuestionnaire, JfSection, JfQuestion, JfAnswerOption });
+Object.assign(module.exports, JustFormsCore);
+Object.assign(module.exports, JfQuestionnaire);
+Object.assign(module.exports, JfSection);
+Object.assign(module.exports, JfQuestion);
+Object.assign(module.exports, JfAnswerOption);
